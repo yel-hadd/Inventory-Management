3494ae596221b89c779161baba92a04364e98726
yaseen-ai <82560247+yaseen-ai@users.noreply.github.com>
2022-04-12 18:49:09 +0000

    stl

--- POS/signin.py
+++ POS/signin.py
@@ -3,7 +3,23 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
 class SigninWindow(BoxLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def validate_user(self):
+        user = self.ids.username_field
+        pwd = self.ids.pwd_field
+        info = self.ids.info
+
+        username = user.text
+        password = pwd.text
+
+        if username == "" or password == "":
+            info.text = "[color=#FF0000]NOM D'UTILISATEUR ET / OU MOT DE PASSE REQUIS[/color]"
+        elif username == 'admin' and password == 'admin':
+            info.text = "[color=#006400]CONNECTÉ AVEC SUCCÈS[/color]"
+        else:
+            info.text = "[color=#FF0000]NOM D'UTILISATEUR ET / OU MOT DE PASSE INVALIDE[/color]"
 
 class SigninApp(App):
     def build(self):
