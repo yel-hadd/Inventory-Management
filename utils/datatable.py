2626c83e13c48616fd91d70abd8d8ee29adaebf7
yaseen-ai <82560247+yaseen-ai@users.noreply.github.com>
2022-05-14 01:28:08 +0100

    added operator window buttons
    print button
    cancel button

--- utils/datatable.py
+++ utils/datatable.py
@@ -13,6 +13,7 @@ Builder.load_string('''
     RecycleView:
         viewclass: 'CustLabel'
         id: table_floor
+        bold: True
         RecycleGridLayout:
             id: table_floor_layout
             cols: 8
@@ -43,117 +44,20 @@ class DataTable(BoxLayout):
         self.columns = len(col_titles)
         table_data = []
         for t in col_titles:
-            table_data.append({'text':str(t), 'size_hint_y':None, 'height':50, 'bcolor':(.06,.45, .45, 1)})
-        
+            if  t == 'designation':
+                table_data.append({'text':str(t), 'size_hint_y':None, 'size_hint_x':5, 'height':50, 'bcolor':(.06,.45, .45, 1)})
+            else:
+                table_data.append({'text':str(t), 'size_hint_y':None, 'height':50, 'bcolor':(.06,.45, .45, 1)})
+
         for r in range(row_len):
             for t in col_titles:
                 table_data.append({'text':str(products[t][r]),
                                    'size_hint_y':None, 'height':40, 'bcolor':(.06,  .25, .25, 1)})
+        
                 
         
         self.ids.table_floor.data = table_data
         self.ids.table_floor_layout.cols  = self.columns
-    
-    def get_users(self):
-        client = MongoClient()
-        db = client.pos
-        users = db.users
-        _users = OrderedDict(
-            first_names = {},
-            last_names = {},
-            user_names = {},
-            passwords = {},
-            designations = {}
-        )
-
-        _users = {}
-        _users['first_names'] = {}
-        _users['last_names'] = {}
-        _users['user_names'] = {}
-        _users['passwords'] = {}
-        _users['designations'] = {}
-        
-
-        first_names = []
-        last_names = []
-        user_names = []
-        passwords = []
-        designations = []
-
-        for user in users.find():
-            first_names.append(user['first_name'])
-            last_names.append(user['last_name'])
-            user_names.append(user['user_name'])
-            passwords.append(user['password'])
-            designations.append(user['designation'])
-
-        for c, v in enumerate(first_names):
-            _users['first_names'][c] = first_names[c]
-            _users['last_names'][c] = last_names[c]
-            _users['user_names'][c] = user_names[c]
-            _users['passwords'][c] = passwords[c]
-            _users['designations'][c] = designations[c]
-        
-        return _users
-
-    def get_products(self):
-        client = MongoClient()
-        db = client.pos
-        products = db.stocks
-        _stocks = {}
-        _stocks['Ref'] = {}
-        _stocks['designation'] = {}
-        _stocks['prix'] = {}
-        _stocks['prix_achat'] = {}
-        _stocks['en_stock'] = {}
-        _stocks['vendu'] = {}
-        _stocks['commande'] = {}
-        _stocks['dernier_achat'] = {}
-
-        Ref = []
-        prix = []
-        prix_achat = []
-        marque = []
-        modele = []
-        cpu = []
-        ram = []
-        gpu = []
-        stockage = []
-        batterie = []
-        en_stock = []
-        vendu = []
-        commande = []
-        dernier_achat = []
-
-        for product in products.find():
-            Ref.append(product['Ref'])
-            prix.append(product['prix'])
-            prix_achat.append(product['prix_achat'])
-            marque.append(product['marque'])
-            modele.append(product['modele'])
-            cpu.append(product['cpu'])
-            ram.append(product['ram'])
-            gpu.append(product['gpu'])
-            stockage.append(product['stockage'])
-            batterie.append(product['batterie'])
-            en_stock.append(product['en_stock'])
-            vendu.append(product['vendu'])
-            commande.append(product['commande'])
-            dernier_achat.append(product['dernier_achat'])
-            
-        for c, v in enumerate(Ref):
-            _stocks['Ref'][c] = Ref[c]
-            _stocks['designation'][c] = f"{marque[c]} {modele[c]} | {cpu[c]} | {ram[c]}GB\n{stockage[c]} | {gpu[c]} | {batterie[c]}"
-            _stocks['prix'][c] = prix[c]
-            _stocks['prix_achat'][c] = prix_achat[c]
-            _stocks['en_stock'][c] = en_stock[c]
-            _stocks['vendu'][c] = vendu[c]
-            _stocks['commande'][c] = commande[c]
-            _stocks['dernier_achat'][c] = dernier_achat[c]
-
-        return _stocks
-    
-
 
 
 #class DataTableApp(App):
